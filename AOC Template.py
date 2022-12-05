dc60b47ca098c255a4d9aead3bb8a1fe8e3b04f9
Baghaskara Pandya Narayana <30591704+Salatmate@users.noreply.github.com>
2022-12-05 19:04:19 +0800

    Updated template

--- AOC Template.py
+++ AOC Template.py
@@ -4,10 +4,14 @@ file = open(sys.argv[0].split('\\')[-1].replace('py','txt'),'r')
 raw_inp = file.read()
 file.close()
 
-def parse(inp):
+def cleanInput(inp,filter):
+    for char in filter:
+        inp = inp.replace(char,' ')
+    return inp
+def parseInput(inp):
+    inp = cleanInput(inp,',-')
     inp = inp.split('\n') #split into lines
     #inp = [int(i) for i in inp] #convert each line into integer
-    #inp = inp.split('\n\n')
     #inp = [[int(j) for j in i.split('\n')] for i in inp]
     #inp = [i.split(' ') for i in inp] #convert each line into list
     #inp = [[int(j) for j in i.split(' ')] for i in inp] #convert each line into list
@@ -20,8 +24,8 @@ def adj8(pos):
     x,y = pos
     return [(x,y-1),(x-1,y-1),(x-1,y),(x-1,y+1),(x,y+1),(x+1,y+1),(x+1,y),(x+1,y-1)]
 
-inp = parse(raw_inp)
-try: test = parse(test)
+inp = parseInput(raw_inp)
+try: test = parseInput(test)
 except Exception: pass
 
 def part1(inp):
