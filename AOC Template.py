aff0bc3a777459c33447552a4938d6eb9196aa3d
Baghaskara Pandya Narayana <30591704+Salatmate@users.noreply.github.com>
2022-12-06 17:06:41 +0800

    Update AOC Template.py

--- AOC Template.py
+++ AOC Template.py
@@ -4,12 +4,41 @@ file = open(sys.argv[0].split('\\')[-1].replace('py','txt'),'r')
 raw_inp = file.read()
 file.close()
 
+def adj4(pos):
+    x,y = pos
+    return [(x,y-1),(x-1,y),(x,y+1),(x+1,y)]
+def adj8(pos):
+    x,y = pos
+    return [(x,y-1),(x-1,y-1),(x-1,y),(x-1,y+1),(x,y+1),(x+1,y+1),(x+1,y),(x+1,y-1)]
+def visual(grid,flipX=False,flipY=False):
+    output = ''
+    positions = list(grid.keys())
+    if not flipY:
+        for y in range(min(positions,key=lambda x:x[1])[1],max(positions,key=lambda x:x[1])[1]):
+            if not flipX:
+                for x in range(min(positions,key=lambda x:x[0])[0],max(positions,key=lambda x:x[0])[0]):
+                    output += grid[(x,y)]
+            else:
+                for x in reversed(range(min(positions,key=lambda x:x[0])[0],max(positions,key=lambda x:x[0])[0])):
+                    output += grid[(x,y)]
+            output += '\n'
+    else:
+        for y in reversed(range(min(positions,key=lambda x:x[1])[1],max(positions,key=lambda x:x[1])[1])):
+            if not flipX:
+                for x in range(min(positions,key=lambda x:x[0])[0],max(positions,key=lambda x:x[0])[0]):
+                    output += grid[(x,y)]
+            else:
+                for x in reversed(range(min(positions,key=lambda x:x[0])[0],max(positions,key=lambda x:x[0])[0])):
+                    output += grid[(x,y)]
+            output += '\n'
+    print(output)
+
 def cleanInput(inp,filter):
     for char in filter:
         inp = inp.replace(char,' ')
     return inp
 def parseInput(inp):
-    inp = cleanInput(inp,',-')
+    inp = cleanInput(inp,',-:')
     inp = inp.split('\n') #split into lines
     #inp = [int(i) for i in inp] #convert each line into integer
     #inp = [[int(j) for j in i.split('\n')] for i in inp]
@@ -17,17 +46,11 @@ def parseInput(inp):
     #inp = [[int(j) for j in i.split(' ')] for i in inp] #convert each line into list
     return inp
 
-def adj4(pos):
-    x,y = pos
-    return [(x,y-1),(x-1,y),(x,y+1),(x+1,y)]
-def adj8(pos):
-    x,y = pos
-    return [(x,y-1),(x-1,y-1),(x-1,y),(x-1,y+1),(x,y+1),(x+1,y+1),(x+1,y),(x+1,y-1)]
-
 inp = parseInput(raw_inp)
 try: test = parseInput(test)
 except Exception: pass
 
+from collections import defaultdict
 def part1(inp):
     ans = 0
     print(ans)
